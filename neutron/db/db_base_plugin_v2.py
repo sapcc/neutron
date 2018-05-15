db139659dd91b8571f883c0b97b756b1da9b6508
Andrew Karpow <andrew.karpow@sap.com>
2018-05-15 15:24:03 +0200

    try to improve get_ports performance

--- neutron/db/db_base_plugin_v2.py
+++ neutron/db/db_base_plugin_v2.py
@@ -1379,6 +1379,12 @@ class NeutronDbPluginV2(db_base_plugin_common.DbBasePluginCommon,
                                                sorts, marker_obj)
         return query
 
+    def _get_dns_name_fast(self, item):
+        if 'dns-integration' in self.supported_extension_aliases and 'dns_name' in item:
+            item['dns_assignment'] = self._get_dns_name_for_port_get(None, item)
+
+        return item
+
     def get_ports(self, context, filters=None, fields=None,
                   sorts=None, limit=None, marker=None,
                   page_reverse=False):
@@ -1387,13 +1393,7 @@ class NeutronDbPluginV2(db_base_plugin_common.DbBasePluginCommon,
                                       sorts=sorts, limit=limit,
                                       marker_obj=marker_obj,
                                       page_reverse=page_reverse)
-        items = []
-        for c in query:
-            if (('dns-integration' in self.supported_extension_aliases and
-                 'dns_name' in c)):
-                c['dns_assignment'] = self._get_dns_name_for_port_get(context,
-                                                                      c)
-            items.append(self._make_port_dict(c, fields))
+        items = [self._get_dns_name_fast(self._make_port_dict(c, fields)) for c in query]
         if limit and page_reverse:
             items.reverse()
         return items
