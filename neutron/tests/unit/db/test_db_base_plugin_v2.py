01bb0a8f7a7d47490560edfef451b602af983009
Sebastian Wagner <sebastian.wagner02@sap.com>
2022-06-28 17:06:53 +0200

    Relax check on subnet pools to just satisfy same address scope
    The current implementation does not allow subnets from different address
    scopes in a network. For DAPnets we need to relax that constraint so we
    allow subnets from the same address scope as well as subnets with no
    subnet pool (and hence no address scope) and subnets with the same
    address scope in a network.

--- neutron/tests/unit/db/test_db_base_plugin_v2.py
+++ neutron/tests/unit/db/test_db_base_plugin_v2.py
@@ -7185,12 +7185,11 @@ class NeutronDbPluginV2AsMixinTestCase(NeutronDbPluginV2TestCase,
         network.subnets = [models_v2.Subnet(subnetpool_id='test_id',
                                             ip_version=constants.IP_VERSION_4)]
         new_subnetpool_id = None
-        self.assertRaises(lib_exc.NetworkSubnetPoolAffinityError,
-                          self.plugin.ipam._validate_network_subnetpools,
-                          network,
+        self.assertIsNone(
+            self.plugin.ipam._validate_network_subnetpools(network,
                           constants.IP_VERSION_4,
                           new_subnetpool_id,
-                          None)
+                          None))
 
     def test_create_subnet_invalid_network_mtu_ipv4_returns_409(self):
         self.net_data['network']['mtu'] = common_constants.IPV4_MIN_MTU - 1
