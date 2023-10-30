edbc2c3fdb6551c025a072589431e10a38a593d2
Sebastian Lohff <sebastian.lohff@sap.com>
2023-10-30 14:36:45 +0100

    Prefer default gw with ip on port on DHCP Agent
    When multiple subnets are configured in the same network, the DHCP agent
    will choose the first suitable subnet with a gateway ip set as default
    gateway, where the subnets are in a generally stable, but arbitrary
    order. In some cases only some of the subnets in a network actually have
    a router in it, meaning a DHCP agent might loose Internet access (used
    for example for DNS).
    
    Now we sort the subnets before selecting a default gateway. Subnets are
    sorted first by if their gateway ip is present on a port in the network
    and then by their created_at time, so we always have a stable order.
    Also, using the first created subnet seems like a good decision, as it
    will probably be the one with the least fluctuations. We won't filter
    for a router port, as the user could also provide a custom router via a
    VM in their network. In case no subnet is present on a port we will
    still use one of them as default route, just to preserve the old
    behavior.

--- neutron/tests/unit/agent/dhcp/test_agent.py
+++ neutron/tests/unit/agent/dhcp/test_agent.py
@@ -74,7 +74,8 @@ fake_subnet1 = dhcp.DictModel(id='bbbbbbbb-bbbb-bbbb-bbbbbbbbbbbb',
                               ip_version=const.IP_VERSION_4,
                               subnetpool_id=FAKE_V4_SUBNETPOOL_ID,
                               ipv6_ra_mode=None, ipv6_address_mode=None,
-                              allocation_pools=fake_subnet1_allocation_pools)
+                              allocation_pools=fake_subnet1_allocation_pools,
+                              created_at='2023-10-30T15:21:46Z')
 
 fake_subnet2_allocation_pools = dhcp.DictModel(id='', start='172.9.8.2',
                                                end='172.9.8.254')
@@ -85,7 +86,8 @@ fake_subnet2 = dhcp.DictModel(id='dddddddd-dddd-dddd-dddddddddddd',
                               gateway_ip='172.9.8.1',
                               host_routes=[], dns_nameservers=[],
                               ip_version=const.IP_VERSION_4,
-                              allocation_pools=fake_subnet2_allocation_pools)
+                              allocation_pools=fake_subnet2_allocation_pools,
+                              created_at='2023-10-30T15:21:46Z')
 
 fake_subnet3 = dhcp.DictModel(id='bbbbbbbb-1111-2222-bbbbbbbbbbbb',
                               network_id=FAKE_NETWORK_UUID,
@@ -1995,11 +1997,13 @@ class TestNetworkCache(base.BaseTestCase):
 class FakePort1:
     def __init__(self):
         self.id = 'eeeeeeee-eeee-eeee-eeee-eeeeeeeeeeee'
+        self.fixed_ips = []
 
 
 class FakePort2:
     def __init__(self):
         self.id = 'ffffffff-ffff-ffff-ffff-ffffffffffff'
+        self.fixed_ips = []
 
 
 class FakeV4Subnet:
@@ -2010,6 +2014,7 @@ class FakeV4Subnet:
         self.gateway_ip = '192.168.0.1'
         self.enable_dhcp = True
         self.subnetpool_id = FAKE_V4_SUBNETPOOL_ID
+        self.created_at = "2023-10-30T15:21:46Z"
 
 
 class FakeV6Subnet:
@@ -2020,12 +2025,14 @@ class FakeV6Subnet:
         self.gateway_ip = '2001:db8:0:1::1'
         self.enable_dhcp = True
         self.subnetpool_id = FAKE_V6_SUBNETPOOL_ID
+        self.created_at = "2023-10-30T15:21:46Z"
 
 
 class FakeV4SubnetOutsideGateway(FakeV4Subnet):
     def __init__(self):
         super().__init__()
         self.gateway_ip = '192.168.1.1'
+        self.created_at = "2023-10-30T15:21:46Z"
 
 
 class FakeV6SubnetOutsideGateway(FakeV6Subnet):
@@ -2041,6 +2048,7 @@ class FakeV4SubnetNoGateway:
         self.cidr = '192.168.1.0/24'
         self.gateway_ip = None
         self.enable_dhcp = True
+        self.created_at = "2023-10-30T15:21:46Z"
 
 
 class FakeV6SubnetNoGateway:
@@ -2050,6 +2058,7 @@ class FakeV6SubnetNoGateway:
         self.cidr = '2001:db8:1:0::/64'
         self.gateway_ip = None
         self.enable_dhcp = True
+        self.created_at = "2023-10-30T15:21:46Z"
 
 
 class FakeV4Network:
